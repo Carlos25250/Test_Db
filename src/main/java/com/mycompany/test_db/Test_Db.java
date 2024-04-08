47da3603485aa3533d197951500d6333b84c51f6
Carlos25250 <2410558@cesba-queretaro.edu.mx>
2024-04-08 09:00:09 -0600

    3 commit

--- src/main/java/com/mycompany/test_db/Test_Db.java
+++ src/main/java/com/mycompany/test_db/Test_Db.java
@@ -5,11 +5,12 @@ package com.mycompany.test_db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+//Carlos gonzalez.@
+//5456455
 
 public class Test_Db{
      public static void main(String[] args) throws ClassNotFoundException, SQLException
- { 
+ {
     try {
         String driver = "com.mysql.cj.jdbc.Driver";
         String url = "jdbc:mysql://localhost:3306/student";
